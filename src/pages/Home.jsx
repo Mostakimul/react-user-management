13bac6f4f9eed58a4ced9204299468a627af0adc
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2022-02-24 13:52:10 +0100

    [FEATURE]: sorting table by username

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import { Spinner, Table } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Button, Col, Row, Spinner, Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import AppLayout from '../components/layout/AppLayout';
 import SingleUser from '../components/singleUser/SingleUser';
 import { fetchUsers } from '../features/user/userSlice';
-import { H2 } from '../styles/Element.styled';
+import { H2, H3 } from '../styles/Element.styled';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -12,11 +12,14 @@ const Home = () => {
     (state) => state.users,
   );
 
+  const [userData, setUserData] = useState([]);
+
   // fetcing user if is not available
   useEffect(() => {
     if (allUsers.length === 0) {
       dispatch(fetchUsers());
     }
+    setUserData(allUsers);
   }, []);
 
   if (isLoading) {
@@ -29,12 +32,47 @@ const Home = () => {
     );
   }
 
+  // A-Z sorting
+  const sortAZ = (fieldType) => {
+    const azSort = [...userData].sort((a, b) => {
+      return a[fieldType].toLowerCase() > b[fieldType].toLowerCase() ? 1 : -1;
+    });
+    setUserData(azSort);
+  };
+  // Z-A sorting
+  const sortZA = (fieldType) => {
+    const azSort = [...userData].sort((a, b) => {
+      return a[fieldType].toLowerCase() < b[fieldType].toLowerCase() ? 1 : -1;
+    });
+    setUserData(azSort);
+  };
+
   if (isError) {
     return <H2>{message}</H2>;
   }
 
   return (
     <AppLayout title="All Users" showAdd>
+      <Row>
+        <Col className="mb-2">
+          <H3>Sort by username: </H3>
+          <Button
+            onClick={() => sortAZ('username')}
+            className="me-2"
+            variant="secondary"
+            size="sm"
+          >
+            A-Z
+          </Button>
+          <Button
+            onClick={() => sortZA('username')}
+            variant="secondary"
+            size="sm"
+          >
+            Z-A
+          </Button>
+        </Col>
+      </Row>
       {allUsers.length <= 0 ? (
         <div className="alert alert-primary" role="alert">
           There is no user!!!
@@ -53,7 +91,7 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            {allUsers.map((user, index) => (
+            {userData.map((user, index) => (
               <SingleUser key={user.id} user={user} index={index} />
             ))}
           </tbody>
--- src/styles/Element.styled.jsx
+++ src/styles/Element.styled.jsx
@@ -5,6 +5,13 @@ export const H2 = styled.h2`
   color: #161616cc;
 `;
 
+export const H3 = styled.h3`
+  font-size: 1rem;
+  display: inline-block;
+  color: #161616cc;
+  margin-right: 0.5rem;
+`;
+
 export const P = styled.p`
   font-size: ${(props) => props.fontSize || '1.6rem'};
   margin: 0.5rem 0;
